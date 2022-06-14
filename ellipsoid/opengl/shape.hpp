6d84c1dce9208e4c9ba9a409db3ab770dbec798a
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-14 02:06:21 -0400

    Initialized the files for the definition of shapes

---
+++ ellipsoid/opengl/shape.hpp
@@ -0,0 +1,20 @@
+// #ifndef ELLIPSOID_GL_SHAPE_HEADER
+// #define ELLIPSOID_GL_SHAPE_HEADER
+
+// #include "buffers/buffers.hpp"
+
+// namespace ellipsoid {
+// namespace gl {
+
+// using namespace buffers;
+
+// class Shape {
+// private:
+//     VAO _vao;
+//     EBO _ebo;
+// };
+
+// } // namespace gl
+// } // namespace ellipsoid
+
+// #endif
