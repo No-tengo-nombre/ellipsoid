2d3761413e4b389de8d3526d0450abadd75f281f
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-14 02:07:53 -0400

    Initialized the files for the creation of textures

---
+++ ellipsoid/opengl/core/texture.hpp
@@ -0,0 +1,16 @@
+#ifndef ELLIPSOID_GL_CORE_TEXTURE_HEADER
+#define ELLIPSOID_GL_CORE_TEXTURE_HEADER
+
+namespace ellipsoid {
+namespace gl {
+namespace core {
+
+class Texture2D {};
+
+class Texture3D {};
+
+} // namespace core
+} // namespace gl
+} // namespace ellipsoid
+
+#endif
