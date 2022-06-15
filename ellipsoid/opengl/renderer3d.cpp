b0fff3844d7ef3dfe9cb88aecfa08d7219025127
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-15 01:53:49 -0400

    Implemented the current methods for the Renderer3D class

---
+++ ellipsoid/opengl/renderer3d.cpp
@@ -0,0 +1,39 @@
+#include "renderer.hpp"
+
+namespace ellipsoid {
+namespace gl {
+
+Renderer3D::Renderer3D() { _clearColor = ColorRGBA(0, 0, 0); }
+
+Renderer3D* Renderer3D::setClearColor(ColorRGBA color) {
+    _clearColor = color;
+    glClearColor(color[0], color[1], color[2], color[3]);
+    return this;
+}
+
+Renderer3D* Renderer3D::add(const Shape item) {
+    _items.push_back(item);
+    return this;
+}
+
+void Renderer3D::render() const {
+    // TODO: Implement batch rendering
+    for (Shape s : _items) {
+        draw(s);
+    }
+}
+
+void Renderer3D::draw(const Shape item) const { item.draw(); }
+
+const Renderer3D* Renderer3D::polygonMode(const GLenum face,
+                                    const GLenum mode) const {
+    glPolygonMode(face, mode);
+    return this;
+}
+
+void Renderer3D::clear() const {
+    glClear(GL_COLOR_BUFFER_BIT || GL_DEPTH_BUFFER_BIT)
+}
+
+} // namespace gl
+} // namespace ellipsoid
