31bf0db6db482e54124795ce9e2ae6da53c1d8f4
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-15 00:14:42 -0400

    Added methods to the Texture2D class

--- ellipsoid/opengl/core/texture.hpp
+++ ellipsoid/opengl/core/texture.hpp
@@ -1,6 +1,7 @@
 #ifndef ELLIPSOID_GL_CORE_TEXTURE_HEADER
 #define ELLIPSOID_GL_CORE_TEXTURE_HEADER
 
+#include <ellipsoid/utils/image.hpp>
 #include <string>
 
 namespace ellipsoid {
@@ -10,16 +11,21 @@ namespace core {
 class Texture2D {
 private:
     unsigned int _id;
+    utils::Image* _image = nullptr;
 
     static void loadTexture(unsigned int& id, const unsigned int slot,
                             const unsigned int width, const unsigned int height,
-                            const unsigned int channels, const char* data);
+                            const unsigned int channels,
+                            const unsigned char* data);
 
 public:
     Texture2D();
     Texture2D(const std::string path);
+    ~Texture2D();
 
     Texture2D* loadFromPath(const std::string path);
+    Texture2D* build();
+    Texture2D* build(unsigned int slot);
 
     void bind() const;
     void unbind() const;
