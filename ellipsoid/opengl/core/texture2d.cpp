a70c66faa673ec3e747664b183ab1b515b547b9c
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-15 00:14:07 -0400

    Separated different implementations of textures

--- ellipsoid/opengl/core/texture2d.cpp
+++ ellipsoid/opengl/core/texture2d.cpp
@@ -8,7 +8,25 @@ namespace core {
 Texture2D::Texture2D() { _id = 0; }
 
 Texture2D::Texture2D(const std::string path) {
-    unsigned int id = 0;
+    utils::Image result = utils::loadImage(path);
+    _id = 0;
+    _image = new utils::Image;
+    _image->load(result._width, result._height, result._channels, result._data);
+}
+
+Texture2D* Texture2D::loadFromPath(const std::string path) {
+    utils::Image result = utils::loadImage(path);
+    _image = new utils::Image;
+    _image->load(result._width, result._height, result._channels, result._data);
+    return this;
+}
+
+Texture2D* Texture2D::build() { return build(0); }
+
+Texture2D* Texture2D::build(unsigned int slot) {
+    loadTexture(_id, slot, _image->_width, _image->_height, _image->_channels,
+                _image->_data);
+    return this;
 }
 
 void Texture2D::bind() const { glBindTexture(GL_TEXTURE_2D, _id); }
@@ -17,7 +35,8 @@ void Texture2D::unbind() const { glBindTexture(GL_TEXTURE_2D, 0); }
 
 void Texture2D::loadTexture(unsigned int& id, const unsigned int slot,
                             const unsigned int width, const unsigned int height,
-                            const unsigned int channels, const char* data) {
+                            const unsigned int channels,
+                            const unsigned char* data) {
     glGenTextures(GL_TEXTURE_2D, &id);
     glActiveTexture(slot);
     glBindTexture(GL_TEXTURE_2D, id);
