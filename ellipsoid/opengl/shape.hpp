8da4d3004a043733660e680bbdde0750bdec42c7
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-15 02:08:39 -0400

    Added the core functions for the Shape class

--- ellipsoid/opengl/shape.hpp
+++ ellipsoid/opengl/shape.hpp
@@ -2,6 +2,10 @@
 #define ELLIPSOID_GL_SHAPE_HEADER
 
 #include "buffers/buffers.hpp"
+#include "material.hpp"
+#include "core/texture.hpp"
+#include <glad/gl.h>
+#include <vector>
 
 namespace ellipsoid {
 namespace gl {
@@ -12,10 +16,43 @@ class Shape {
 private:
     VAO _vao;
     EBO _ebo;
-    GLenum _drawMode;
+    Material _material;
+    Texture2D _texture;
+    std::vector<float> _vertices;
+    std::vector<unsigned int> _indices;
+    std::vector<unsigned int> _layouts;
+    std::vector<unsigned int> _vertSizes;
+    unsigned int _count;
+    GLenum _usage = GL_STATIC_DRAW;
+    GLenum _drawMode = GL_TRIANGLES;
 
 public:
+    Shape();
+
     GLenum getDrawMode() const { return _drawMode; };
+
+    Shape* material(const Material material);
+    Shape* texture(const Texture2D texture);
+    Shape* vertices(const std::vector<float> verts);
+    Shape* indices(const std::vector<unsigned int> inds);
+    Shape* layouts(const std::vector<unsigned int> lays);
+    Shape* vertSizes(const std::vector<unsigned int> sizes);
+    Shape* count(const unsigned int newCount);
+    Shape* usage(const GLenum newUsage);
+    Shape* drawMode(const GLenum newDrawMode);
+
+    Shape* build();
+
+    void useMaterial() const;
+    void bindVAO() const;
+    void bindEBO() const;
+    void bindTexture() const;
+    void unbindVAO() const;
+    void unbindEBO() const;
+    void unbindTexture() const;
+
+    void del() const;
+
     void draw() const;
 };
 
