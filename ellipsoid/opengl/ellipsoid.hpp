a34a6d168eb8b52a164e68a056b51428fe46d7b0
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-16 00:40:27 -0400

    Added a header that includes most of the basic files

---
+++ ellipsoid/opengl/ellipsoid.hpp
@@ -0,0 +1,13 @@
+#ifndef ELLIPSOID_GL_ELLIPSOID_HEADER
+#define ELLIPSOID_GL_ELLIPSOID_HEADER
+
+#include <ellipsoid/opengl/camera.hpp>
+#include <ellipsoid/opengl/core/shader.hpp>
+#include <ellipsoid/opengl/core/texture.hpp>
+#include <ellipsoid/opengl/core/transform.hpp>
+#include <ellipsoid/opengl/core/window.hpp>
+#include <ellipsoid/opengl/material.hpp>
+#include <ellipsoid/opengl/renderer.hpp>
+#include <ellipsoid/opengl/shape.hpp>
+
+#endif
