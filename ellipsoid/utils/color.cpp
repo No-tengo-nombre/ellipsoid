e9f988eb73197aa3c729baf388a912c04b75c09c
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-06-19 21:06:25 -0400

    Added RGBA colors with floats instead of 8 bit ints

--- ellipsoid/utils/color.cpp
+++ ellipsoid/utils/color.cpp
@@ -3,24 +3,24 @@
 namespace ellipsoid {
 namespace utils {
 
-unsigned int ColorRGBA::limitValue(unsigned int value) {
-    if (ELLIPSOID_COLOR_MIN <= value) {
-        if (value <= ELLIPSOID_COLOR_MAX) {
+unsigned int ColorRGBA255::limitValue(unsigned int value) {
+    if (ELLIPSOID_COLOR255_MIN <= value) {
+        if (value <= ELLIPSOID_COLOR255_MAX) {
             return value;
         }
-        return ELLIPSOID_COLOR_MAX;
+        return ELLIPSOID_COLOR255_MAX;
     }
-    return ELLIPSOID_COLOR_MIN;
+    return ELLIPSOID_COLOR255_MIN;
 }
 
-ColorRGBA::ColorRGBA() {
+ColorRGBA255::ColorRGBA255() {
     _r = 0;
     _g = 0;
     _b = 0;
     _a = 0;
 }
 
-ColorRGBA::ColorRGBA(const unsigned int r, const unsigned int g,
+ColorRGBA255::ColorRGBA255(const unsigned int r, const unsigned int g,
                      const unsigned int b) {
     _r = limitValue(r);
     _g = limitValue(g);
@@ -28,7 +28,7 @@ ColorRGBA::ColorRGBA(const unsigned int r, const unsigned int g,
     _a = 255;
 }
 
-ColorRGBA::ColorRGBA(const unsigned int r, const unsigned int g,
+ColorRGBA255::ColorRGBA255(const unsigned int r, const unsigned int g,
                      const unsigned int b, const unsigned int a) {
     _r = limitValue(r);
     _g = limitValue(g);
@@ -36,20 +36,24 @@ ColorRGBA::ColorRGBA(const unsigned int r, const unsigned int g,
     _a = limitValue(a);
 }
 
-void ColorRGBA::r(unsigned int val) { _r = limitValue(val); }
+void ColorRGBA255::r(const unsigned int val) { _r = limitValue(val); }
 
-void ColorRGBA::g(unsigned int val) { _g = limitValue(val); }
+void ColorRGBA255::g(const unsigned int val) { _g = limitValue(val); }
 
-void ColorRGBA::b(unsigned int val) { _b = limitValue(val); }
+void ColorRGBA255::b(const unsigned int val) { _b = limitValue(val); }
 
-void ColorRGBA::a(unsigned int val) { _a = limitValue(val); }
+void ColorRGBA255::a(const unsigned int val) { _a = limitValue(val); }
 
-std::vector<unsigned int> ColorRGBA::asVector() const {
+std::vector<unsigned int> ColorRGBA255::asVector() const {
     return std::vector<unsigned int>({_r, _g, _b, _a});
 }
 
-const ColorRGBA ColorRGBA::operator+(const ColorRGBA& rhs) {
-    ColorRGBA result;
+ColorRGBAfloat ColorRGBA255::asRGBAfloat() const {
+    return ColorRGBAfloat(_r / 255, _g / 255, _b / 255, _a / 255);
+}
+
+const ColorRGBA255 ColorRGBA255::operator+(const ColorRGBA255& rhs) {
+    ColorRGBA255 result;
     result.r(getR() + rhs.getR());
     result.g(getG() + rhs.getG());
     result.b(getB() + rhs.getB());
@@ -57,8 +61,8 @@ const ColorRGBA ColorRGBA::operator+(const ColorRGBA& rhs) {
     return result;
 }
 
-const ColorRGBA ColorRGBA::operator-(const ColorRGBA& rhs) {
-    ColorRGBA result;
+const ColorRGBA255 ColorRGBA255::operator-(const ColorRGBA255& rhs) {
+    ColorRGBA255 result;
     result.r(getR() - rhs.getR());
     result.g(getG() - rhs.getG());
     result.b(getB() - rhs.getB());
@@ -66,7 +70,7 @@ const ColorRGBA ColorRGBA::operator-(const ColorRGBA& rhs) {
     return result;
 }
 
-ColorRGBA& ColorRGBA::operator+=(const ColorRGBA& rhs) {
+ColorRGBA255& ColorRGBA255::operator+=(const ColorRGBA255& rhs) {
     r(_r + rhs.getR());
     g(_g + rhs.getG());
     b(_b + rhs.getB());
@@ -74,7 +78,7 @@ ColorRGBA& ColorRGBA::operator+=(const ColorRGBA& rhs) {
     return *this;
 }
 
-ColorRGBA& ColorRGBA::operator-=(const ColorRGBA& rhs) {
+ColorRGBA255& ColorRGBA255::operator-=(const ColorRGBA255& rhs) {
     r(_r - rhs.getR());
     g(_g - rhs.getG());
     b(_b - rhs.getB());
@@ -82,7 +86,92 @@ ColorRGBA& ColorRGBA::operator-=(const ColorRGBA& rhs) {
     return *this;
 }
 
-unsigned int& ColorRGBA::operator[](size_t index) {
+unsigned int& ColorRGBA255::operator[](size_t index) {
+    return asVector()[index];
+}
+
+
+
+float ColorRGBAfloat::limitValue(float value) {
+    if (ELLIPSOID_COLOR_FLOAT_MIN <= value) {
+        if (value <= ELLIPSOID_COLOR_FLOAT_MAX) {
+            return value;
+        }
+        return ELLIPSOID_COLOR_FLOAT_MAX;
+    }
+    return ELLIPSOID_COLOR_FLOAT_MIN;
+}
+
+ColorRGBAfloat::ColorRGBAfloat() {
+    _r = 0.0f;
+    _g = 0.0f;
+    _b = 0.0f;
+    _a = 0.0f;
+}
+
+ColorRGBAfloat::ColorRGBAfloat(const float r, const float g,
+                     const float b) {
+    _r = limitValue(r);
+    _g = limitValue(g);
+    _b = limitValue(b);
+    _a = 1.0f;
+}
+
+ColorRGBAfloat::ColorRGBAfloat(const float r, const float g,
+                     const float b, const float a) {
+    _r = limitValue(r);
+    _g = limitValue(g);
+    _b = limitValue(b);
+    _a = limitValue(a);
+}
+
+void ColorRGBAfloat::r(float val) { _r = limitValue(val); }
+
+void ColorRGBAfloat::g(float val) { _g = limitValue(val); }
+
+void ColorRGBAfloat::b(float val) { _b = limitValue(val); }
+
+void ColorRGBAfloat::a(float val) { _a = limitValue(val); }
+
+std::vector<float> ColorRGBAfloat::asVector() const {
+    return std::vector<float>({_r, _g, _b, _a});
+}
+
+const ColorRGBAfloat ColorRGBAfloat::operator+(const ColorRGBAfloat& rhs) {
+    ColorRGBAfloat result;
+    result.r(getR() + rhs.getR());
+    result.g(getG() + rhs.getG());
+    result.b(getB() + rhs.getB());
+    result.a(getA() + rhs.getA());
+    return result;
+}
+
+const ColorRGBAfloat ColorRGBAfloat::operator-(const ColorRGBAfloat& rhs) {
+    ColorRGBAfloat result;
+    result.r(getR() - rhs.getR());
+    result.g(getG() - rhs.getG());
+    result.b(getB() - rhs.getB());
+    result.a(getA() - rhs.getA());
+    return result;
+}
+
+ColorRGBAfloat& ColorRGBAfloat::operator+=(const ColorRGBAfloat& rhs) {
+    r(_r + rhs.getR());
+    g(_g + rhs.getG());
+    b(_b + rhs.getB());
+    a(_a + rhs.getA());
+    return *this;
+}
+
+ColorRGBAfloat& ColorRGBAfloat::operator-=(const ColorRGBAfloat& rhs) {
+    r(_r - rhs.getR());
+    g(_g - rhs.getG());
+    b(_b - rhs.getB());
+    a(_a - rhs.getA());
+    return *this;
+}
+
+float& ColorRGBAfloat::operator[](size_t index) {
     return asVector()[index];
 }
 
--- ellipsoid/utils/color.hpp
+++ ellipsoid/utils/color.hpp
@@ -1,15 +1,53 @@
 #ifndef ELLIPSOID_UTILS_COLOR_HEADER
 #define ELLIPSOID_UTILS_COLOR_HEADER
 
-#define ELLIPSOID_COLOR_MIN 0
-#define ELLIPSOID_COLOR_MAX 255
+#define ELLIPSOID_COLOR255_MIN 0
+#define ELLIPSOID_COLOR255_MAX 255
+#define ELLIPSOID_COLOR_FLOAT_MIN 0.0f
+#define ELLIPSOID_COLOR_FLOAT_MAX 1.0f
 
 #include <vector>
 
 namespace ellipsoid {
 namespace utils {
 
-class ColorRGBA {
+// TODO: Implement polymorphism to simplify colors
+class ColorRGBAfloat {
+private:
+    float _r;
+    float _g;
+    float _b;
+    float _a;
+
+    static float limitValue(const float value);
+
+public:
+    ColorRGBAfloat();
+    ColorRGBAfloat(const float r, const float g, const float b);
+    ColorRGBAfloat(const float r, const float g, const float b,
+              const float a);
+
+    void r(const float val);
+    void g(const float val);
+    void b(const float val);
+    void a(const float val);
+
+    float getR() const { return _r; };
+    float getG() const { return _g; };
+    float getB() const { return _b; };
+    float getA() const { return _a; };
+
+    std::vector<float> asVector() const;
+
+    const ColorRGBAfloat operator+(const ColorRGBAfloat& rhs);
+    const ColorRGBAfloat operator-(const ColorRGBAfloat& rhs);
+    ColorRGBAfloat& operator+=(const ColorRGBAfloat& rhs);
+    ColorRGBAfloat& operator-=(const ColorRGBAfloat& rhs);
+
+    float& operator[](size_t index);
+};
+
+class ColorRGBA255 {
 private:
     unsigned int _r;
     unsigned int _g;
@@ -19,15 +57,15 @@ private:
     static unsigned int limitValue(const unsigned int value);
 
 public:
-    ColorRGBA();
-    ColorRGBA(const unsigned int r, const unsigned int g, const unsigned int b);
-    ColorRGBA(const unsigned int r, const unsigned int g, const unsigned int b,
+    ColorRGBA255();
+    ColorRGBA255(const unsigned int r, const unsigned int g, const unsigned int b);
+    ColorRGBA255(const unsigned int r, const unsigned int g, const unsigned int b,
               const unsigned int a);
 
-    void r(unsigned int val);
-    void g(unsigned int val);
-    void b(unsigned int val);
-    void a(unsigned int val);
+    void r(const unsigned int val);
+    void g(const unsigned int val);
+    void b(const unsigned int val);
+    void a(const unsigned int val);
 
     unsigned int getR() const { return _r; };
     unsigned int getG() const { return _g; };
@@ -35,11 +73,12 @@ public:
     unsigned int getA() const { return _a; };
 
     std::vector<unsigned int> asVector() const;
+    ColorRGBAfloat asRGBAfloat() const;
 
-    const ColorRGBA operator+(const ColorRGBA& rhs);
-    const ColorRGBA operator-(const ColorRGBA& rhs);
-    ColorRGBA& operator+=(const ColorRGBA& rhs);
-    ColorRGBA& operator-=(const ColorRGBA& rhs);
+    const ColorRGBA255 operator+(const ColorRGBA255& rhs);
+    const ColorRGBA255 operator-(const ColorRGBA255& rhs);
+    ColorRGBA255& operator+=(const ColorRGBA255& rhs);
+    ColorRGBA255& operator-=(const ColorRGBA255& rhs);
 
     unsigned int& operator[](size_t index);
 };
